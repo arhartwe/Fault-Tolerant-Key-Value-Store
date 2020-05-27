2917e63c9057527a88314c6f3191b2705628ad31
Bobby Mushkot <bobby.mushkot@gmail.com>
2020-05-26 18:00:06 -0700

    /shard-ids works

--- server/shard.py
+++ server/shard.py
@@ -10,6 +10,12 @@ headers = {'Content-Type': 'application/json'}
 
 shard_api = Blueprint('shard_api', __name__)
 
-@shard_api.route("key-value-store-shard/")
-def placeholder():
-    print("Hello")
+@shard_api.route("/key-value-store-shard/shard-ids", methods=['GET'])
+def shard_id():
+    if request.method == 'GET':
+        response = {}
+        response['message'] = "Shard IDs retrieved successfully"
+        response['shard-ids'] = vars.shard_ids
+        return make_response(response, 200)
+
+#@shard_api.route("/key-value-store-shard/node-shard-id")
--- server/vars.py
+++ server/vars.py
@@ -4,12 +4,17 @@ import sys, os
 # Replica values
 view = os.environ.get('VIEW')
 socket_address = os.environ.get('SOCKET_ADDRESS')
+replica_id = socket_address
 headers = {'Content-Type': 'application/json'}    
+shard_count = int(os.environ.get('SHARD_COUNT'))
 
 # Replica objects
 key_store = {"init": 0}
 local_clock = Counter()
 view_list = view.split(',')
 queue = []
+replication = len(view_list) // shard_count
+shard_list = [view_list[i:i+replication] for i in range(0, len(view_list), replication)]
+shard_ids = [i for i in range(0, len(shard_list))] 
 for each in view_list:
     local_clock[each] = 0
